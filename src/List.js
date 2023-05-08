18d2460c2ee65c5346a830b3e4f96e6f68bfaaef
jvelaequiz <jvelaequiz@educacion.navarra.es>
2023-05-08 12:02:15 +0200

    finak

--- src/List.js
+++ src/List.js
@@ -3,15 +3,35 @@ import React from 'react'
 const List = (props) => {
 	const { jugadores } = props
 	return jugadores.map((person) => {
-		const { dorsal, nombre, edad, posicion,imagen } = person
+		const { dorsal, nombre, edad, posicion,imagen,fechanacimiento } = person
 		return (
 			<article key={dorsal} className="person">
-				<img src={imagen} alt={nombre} />
-				<div>
-					<h4>{nombre}</h4>
-					<span>{posicion}</span>
-					<p>{edad} años</p>
-					
+				
+				<div >
+					<table class="table-container">
+						<tr>
+							<th span='4' class="imagenn"> 
+								<img src={imagen} alt={nombre} />
+							</th>
+							<td>
+								<tr>
+									<td span='2'><span class="nombre">{nombre}</span> <span class="dorsal">{dorsal}</span></td>			
+								</tr>
+								<tr>
+									<th>Posicion:</th>
+									<td> {posicion}</td>
+								</tr>
+								<tr>
+									<th>Edad: </th>
+									<td>{edad} años</td>
+								</tr>
+								<tr>
+									<th>Fecha de Nacimiento:</th>
+									<td>{fechanacimiento}</td>
+								</tr>
+							</td>
+						</tr>				
+					</table>			
 				</div>
 			</article>
 		)
--- src/data.js
+++ src/data.js
@@ -4,51 +4,81 @@ const data = [
 		nombre: 'Sergio Herrera',
 		edad: 29,
 		posicion:'portero',
-		imagen: 'https://assets.laliga.com/squad/2022/t450/p138039/256x278/p138039_t450_2022_1_001_000.png'
+		imagen: 'https://assets.laliga.com/squad/2022/t450/p138039/256x278/p138039_t450_2022_1_001_000.png',
+		fechanacimiento:'23-06-1994'
 	},
 	{
 		dorsal: 2,
 		nombre: 'Nacho Vidal',
 		edad: 28,
 		posicion:'defensa',
-		imagen: 'https://assets.laliga.com/squad/2022/t450/p230598/256x278/p230598_t450_2022_1_001_000.png'
+		imagen: 'https://assets.laliga.com/squad/2022/t450/p230598/256x278/p230598_t450_2022_1_001_000.png',
+		fechanacimiento:'23-06-1994'
 	},
 	{
 		dorsal: 3,
 		nombre: 'Juan Cruz',
 		edad: 30,
 		posicion:'defensa',
-		imagen: 'https://assets.laliga.com/squad/2022/t450/p93203/256x278/p93203_t450_2022_1_001_000.png'
+		imagen: 'https://assets.laliga.com/squad/2022/t450/p93203/256x278/p93203_t450_2022_1_001_000.png',
+		fechanacimiento:'23-06-1994'
 	},
 	{
 		dorsal: 4,
 		nombre: 'Unai Garcia',
 		edad: 31,
 		posicion:'defensa',
-		imagen: 'https://assets.laliga.com/squad/2022/t450/p163711/256x278/p163711_t450_2022_1_001_000.png'
+		imagen: 'https://assets.laliga.com/squad/2022/t450/p163711/256x278/p163711_t450_2022_1_001_000.png',
+		fechanacimiento:'23-06-1994'
 	},
 	{
 		dorsal: 5,
 		nombre: 'David Garcia',
 		edad: 29,
 		posicion:'defensa',
-		imagen: 'https://assets.laliga.com/squad/2022/t450/p98851/256x278/p98851_t450_2022_1_001_000.png'
+		imagen: 'https://assets.laliga.com/squad/2022/t450/p98851/256x278/p98851_t450_2022_1_001_000.png',
+		fechanacimiento:'23-06-1994'
 	},
 	{
 		dorsal: 6,
 		nombre: 'Lucas Torro',
 		edad: 28,
 		posicion:'centrocampista',
-		imagen: 'https://assets.laliga.com/squad/2022/t450/p111232/256x278/p111232_t450_2022_1_001_000.png'
+		imagen: 'https://assets.laliga.com/squad/2022/t450/p111232/256x278/p111232_t450_2022_1_001_000.png',
+		fechanacimiento:'23-06-1994'
 	},
 	{
 		dorsal: 7,
 		nombre: 'Jon Moncayola',
 		edad: 24,
 		posicion:'centrocampista',
-		imagen: 'https://assets.laliga.com/squad/2022/t450/p477463/512x512/p477463_t450_2022_1_003_000.png'
+		imagen: 'https://assets.laliga.com/squad/2022/t450/p477463/512x512/p477463_t450_2022_1_003_000.png',
+		fechanacimiento:'23-06-1994'
+	},
+	{
+		dorsal: 9,
+		nombre: 'Ezequiel Ávila',
+		edad: 30,
+		posicion:'Delantero',
+		imagen: 'https://assets.laliga.com/squad/2022/t450/p205992/256x278/p205992_t450_2022_1_001_000.png',
+		fechanacimiento:'06-02-1994'
+	},
+	{
+		dorsal: 16,
+		nombre: 'Moi Gomez',
+		edad: 29,
+		posicion:'centrocampista',
+		imagen: 'https://assets.laliga.com/squad/2022/t450/p109243/256x278/p109243_t450_2022_1_001_000.png',
+		fechanacimiento:'23-06-1994'
+	},
+	{
+		dorsal: 22,
+		nombre: 'Aimar Oroz',
+		edad: 23,
+		posicion:'centrocampista',
+		imagen: 'https://assets.laliga.com/squad/2022/t450/p484702/256x278/p484702_t450_2022_1_001_000.png',
+		fechanacimiento:'27-11-2001'
 	}
-
 ]
 
 export default data;
